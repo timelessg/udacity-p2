c00d44dbdec7b1ad217d646d0aef30c8c0e0ed5b
Garry <timelessg@hotmail.com>
2017-12-24 22:12:05 +0800

    +again

--- media.py
+++ media.py
@@ -1,6 +1,9 @@
+# coding:utf-8  
+
 class Movie():
-	"""docstring for ClassName"""
+	"""Movie模型类"""
 	def __init__(self, title,poster_image_url,trailer_url):
-		self.title = title
-		self.poster_image_url = poster_image_url
-		self.trailer_url = trailer_url
+		 """Movie构造方法"""
+		 self.title = title #电影名称
+		 self.poster_image_url = poster_image_url #电影海报URL
+		 self.trailer_url = trailer_url #电影预告URL
